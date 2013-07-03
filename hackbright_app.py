e2a90302d77b0d3c588e89f15380005f80e18bd7
Kate <kate@dhcp84.int.hackbrightacademy.com>
2013-07-03 11:06:33 -0700

    added handling for quote marks

--- hackbright_app.py
+++ hackbright_app.py
@@ -1,23 +1,24 @@
 import sqlite3
+import re
 
 DB = None
 CONN = None
 
-
+#given a github acct, print corresponding student name
 def get_student_by_github(github):
-    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
+    query = """SELECT first_name, last_name, github 
+                FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
     row = DB.fetchone()
-    print """\
-Student: %s %s
-Github account: %s"""%(row[0], row[1], row[2])
+    print """Student: %s %s 
+    Github account: %s"""%(row[0], row[1], row[2])
+
 
 def get_project_by_title(title):
     query = """SELECT * FROM Projects WHERE title=?"""
     DB.execute(query, (title,))
     row = DB.fetchone()
-    print """\
-    Title: %s
+    print """Title: %s
     Description: %s
     Max Score: %s""" % (row[0], row[1], row[2])
 
@@ -74,10 +75,33 @@ def connect_to_db():
 
 def split_into_command_and_args(text):
     tokens = text.split()
+    command = None
+    args = []
     if tokens:
         command = tokens[0]
-    args = []
-    temp = None
+    if len(tokens) > 1:
+        args = tokens[1:]
+
+    # deal with splitting on quote marks if used
+    # TODO: the below code is gross...there must be a more elegant way?
+    print "args starts as:",args
+    if re.search("[\"]", text):
+        s = " ".join(args)
+        args = s.split("\"")
+        print "splitting on quotes, args is now:", args
+        i=0
+        while i < len(args):
+            print "Word is:", args[i]
+            if args[i] == "":
+                args.pop(i)
+            else:
+                if args[i][0] == " ":
+                    args[i] = args[i][1:]
+                if args[i][-1] == " ":
+                    args[i] = args[i][0:-1]
+            i += 1
+
+    return (command, args)
 
 
 def main():
@@ -85,12 +109,8 @@ def main():
     command = None
     while command != "quit":
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split()
-
-
-
-        command = tokens[0]
-        args = tokens[1:]
+        
+        command, args = split_into_command_and_args(input_string)
 
         if command == "student":
             get_student_by_github(*args) 
@@ -108,6 +128,8 @@ def main():
             get_grades_by_name(*args)
         elif command == "new_project":
             make_new_project(args[0], " ".join(args[1:-1]),int(args[-1]))
+        else:
+            print "Derp, I don't know what that means. Try again!"
 
     CONN.close()
 
